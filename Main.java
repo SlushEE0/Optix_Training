d7bea267344fd809306ccb2c31b8eb2a8bf60250
neeladem2009 <neeladem2009@gmail.com>
2024-06-18 18:08:10 +0000

    feat: prettified

--- Main.java
+++ Main.java
@@ -1,3 +1,14 @@
 void main() {
+    System.out.println("\u001B[31m\n\n\n");
+    yourCode();
+    System.out.println("\n\n\n");
+}
+
+
+
+
+
+void yourCode() {
     // Code Here
+    System.out.println("hellow");
 }
